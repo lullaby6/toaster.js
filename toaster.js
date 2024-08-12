0880abf54405f3cdc3958cf2511e0de48239be8f
LucianoBrumer <lucianobrumer5@gmail.com>
2024-08-12 17:38:10 -0300

    default toast

--- toaster.js
+++ toaster.js
@@ -10,7 +10,7 @@
 // - Custom Classes
 
 function Toaster({
-    type = 'info',
+    type = 'default',
     text = '',
     position = 'bottom-right',
 
@@ -35,6 +35,7 @@ function Toaster({
     showCloseIcon = true,
     onlyShowCloseIconOnHover = false,
 
+    defaultIcon = null,
     infoIcon = `
         <svg xmlns="http://www.w3.org/2000/svg" width="1em"  height="1em"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-info-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 2c5.523 0 10 4.477 10 10a10 10 0 0 1 -19.995 .324l-.005 -.324l.004 -.28c.148 -5.393 4.566 -9.72 9.996 -9.72zm0 9h-1l-.117 .007a1 1 0 0 0 0 1.986l.117 .007v3l.007 .117a1 1 0 0 0 .876 .876l.117 .007h1l.117 -.007a1 1 0 0 0 .876 -.876l.007 -.117l-.007 -.117a1 1 0 0 0 -.764 -.857l-.112 -.02l-.117 -.006v-3l-.007 -.117a1 1 0 0 0 -.876 -.876l-.117 -.007zm.01 -3l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007z" /></svg>
     `,
@@ -60,6 +61,7 @@ function Toaster({
     }
 
     const icons = {
+        default: defaultIcon,
         info: infoIcon,
         success: successIcon,
         warning: warningIcon,
@@ -122,7 +124,7 @@ function Toaster({
 
     div.innerHTML = `
         <div class="toaster toaster-${type} toaster-${uuid}">
-            ${showToastIcon ? `
+            ${(showToastIcon && icons[type]) ? `
                 <div class="toaster-icon">
                     ${icons[type]}
                 </div>
